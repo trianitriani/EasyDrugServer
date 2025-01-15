2565230f3c4ae2a3dc5c8fcc8729a3bb721c993f
Lorenzo Valtriani <lorenzovaltriani@gmail.com>
2025-01-15 14:13:44 +0100

    dump

---
+++ src/main/java/it/unipi/EasyDrugServer/EasyDrugServerApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.EasyDrugServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDrugServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDrugServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unipi/EasyDrugServer/EasyDrugServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.EasyDrugServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDrugServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
